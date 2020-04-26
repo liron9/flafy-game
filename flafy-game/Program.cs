35246f3e5faff927881f2d358f72928df7ec8cd6
liron9 <liron.margolis@gmail.com>
2020-04-26 20:51:35 +0300

    player view added

--- flafy-game/Program.cs
+++ flafy-game/Program.cs
@@ -4,9 +4,33 @@ namespace flafy_game
 {
     class Program
     {
+        static void PlayerView(int x, int y)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            string[,] player = { { " ", " ", " ", " ", " ", " ", " ", "_", "_", "_", "_", "_", "_", " " },
+                                 { " ", "_", "_", " ", " ", " ", "|", " ", "o", " ", " ", " ", "o", "|" },
+                                 { " ", " ", " ", "|", "-", "-", "|", " ", " ", " ", "^", " ", " ", "|" },
+                                 { " ", "‾", "‾", " ", " ", " ", "|", " ", " ", " ", "0", " ", " ", "|" },
+                                 { " ", " ", " ", " ", " ", " ", " ", "‾", "‾", "‾", "‾", "‾", "‾", " " } };
+           
+            Console.SetCursorPosition(x, y);
+
+            for (int i = 0; i < player.GetLength(0); i++)
+            {
+                for (int j = 0; j < player.GetLength(1); j++)
+                {
+                    Console.SetCursorPosition(j + x,i + y);
+                    Console.Write(player[i,j]);
+                }
+                Console.WriteLine(" ");
+            } 
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Liron");
+            PlayerView(8, 11);
+            Console.ReadKey();
+
         }
     }
 }
